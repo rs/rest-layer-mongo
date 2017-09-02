977af5fb3c7b70b76df49877d8019a56357d3093
Sindre Røkenes Myren <sindre@searis.no>
2017-09-02 08:57:19 +0200

    Dropped dependency on testify from id_test.go

--- id_test.go
+++ id_test.go
@@ -6,8 +6,6 @@ import (
 
 	"gopkg.in/mgo.v2/bson"
 
-	"github.com/stretchr/testify/assert"
-
 	mongo "github.com/rs/rest-layer-mongo"
 )
 
@@ -17,36 +15,56 @@ const (
 )
 
 func TestObjectIDValidate(t *testing.T) {
-
 	v := &mongo.ObjectID{}
 
 	t.Run("validObjectID", func(t *testing.T) {
+		expect := bson.ObjectIdHex(validObjectID)
 		id, err := v.Validate(validObjectID)
-		assert.Equal(t, bson.ObjectIdHex(validObjectID), id, "v.Validate(validObjectID)")
-		assert.NoError(t, err, "v.Validate(validObjectID)")
+		if expect != id {
+			t.Errorf("v.Validate(validObjectID):\n %v (expect) != %v (actual)", expect, id)
+		}
+		if err != nil {
+			t.Error("v.Validate(validObjectID):\n unexpected error:", err)
+		}
 	})
 
 	t.Run("invalidObjectID", func(t *testing.T) {
 		id, err := v.Validate(invalidObjectID)
-		assert.Nil(t, id, "v.Validate(invalidObjectID)")
-		assert.Error(t, err, "v.Validate(invalidObjectID)")
+		if nil != id {
+			t.Errorf("v.Validate(invalidObjectID):\n %v (expect) != %v (actual)", nil, id)
+		}
+		if err == nil {
+			t.Error("v.Validate(invalidObjectID):\n expected error, got nil")
+		}
 	})
 }
 
 func TestObjectIDJSONSchmea(t *testing.T) {
-
 	v := &mongo.ObjectID{}
 	m, err := v.BuildJSONSchema()
-	assert.NoError(t, err)
-	assert.Equal(t, m["type"], "string")
+	if err != nil {
+		t.Error("_, err := v.BuildJSONSchema():\n unexpected error:", err)
+	}
+	if m == nil {
+		t.Fatal("m, _ := v.BuildJSONSchema():\n expected m not to be nil")
+	}
+	if s := m["type"]; s != "string" {
+		t.Fatalf("m, _ := v.BuildJSONSchema(); m[\"type\"]\n %v (expected) != %v (actual)", "string", s)
+	}
 	re, err := regexp.Compile(m["pattern"].(string))
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal("_, err := regexp.Compile(m[\"type\"]);\n unexpected error:", m, err)
+	}
 
 	t.Run("validObjectID", func(t *testing.T) {
-		assert.True(t, re.MatchString(validObjectID), "re.Match(validObjectID)")
+		if match := re.MatchString(validObjectID); !match {
+			t.Errorf("re.MatchString(validObjectID)\n %v (expected) != %v (actual)", true, match)
+		}
 	})
 
 	t.Run("invalidObjectID", func(t *testing.T) {
-		assert.False(t, re.MatchString(invalidObjectID), "re.Match(invalidObjectID)")
+		if match := re.MatchString(invalidObjectID); match {
+			t.Errorf("re.MatchString(invalidObjectID)\n %v (expected) != %v (actual)", false, match)
+		}
 	})
 }
